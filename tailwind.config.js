f6e38ca5e46165ca12d1058ce556322223e4f296
gedesurya125 <gedesurya125@gmail.com>
2023-12-26 00:47:53 +0800

    installing clsx and setup the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   // https://tailwindcss.com/docs/content-configuration
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/view/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   // https://tailwindcss.com/docs/theme
   theme: {
@@ -24,17 +24,55 @@ module.exports = {
       body: ["var(--font-inter)"],
     },
     extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        tertiary: "var(--color-tertiary)",
+        accent: "var(--color-accent)",
+        background: "var(--color-background)",
+      },
       spacing: {
         "card-sm": "1rem",
         "card-md": "2rem",
         "card-lg": "3rem",
-        "container-phone": "30rem",
+        // GridContainer
+        "container-phone": "33rem",
         "container-tablet-portrait": "80rem",
         "container-tablet-landscape": "100rem",
         "container-desktop": "130rem",
       },
       gridTemplateColumns: {
         24: "repeat(24, 1fr)",
+      },
+      gridColumnEnd: {
+        14: "14",
+        15: "15",
+        16: "16",
+        17: "17",
+        18: "18",
+        19: "19",
+        20: "20",
+        21: "21",
+        22: "22",
+        23: "23",
+      },
+      minHeight: {
+        1: "1rem",
+        2: "2rem",
+        4: "4rem",
+        6: "6rem",
+        8: "8rem",
+        10: "10rem",
+        12: "12rem",
+        14: "14rem",
+        16: "16rem",
+        18: "18rem",
+        20: "20rem",
+        22: "22rem",
+        24: "24rem",
+        26: "26rem",
+        28: "28rem",
+        30: "30rem",
       },
     },
   },
